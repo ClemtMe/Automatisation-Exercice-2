e343b32ae6178501fc71e653245098645712fdc8
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2026-01-17 09:57:37 +0100

    Déplace les fichiers dans assets dans src

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 const modal = document.querySelector('#modal-wrapper');
 const modalContent = document.querySelector('#modal-content');
 const modalTitle = document.querySelector('#modal-title');
